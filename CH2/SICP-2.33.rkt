3827f7a184f340c47e979ae378977877b35c3ffa
GTxx <xiongxiong1986@gmail.com>
2013-07-15 15:21:48 +0800

    Modify append and length, no need to judge if x is null.

--- CH2/SICP-2.33.rkt
+++ CH2/SICP-2.33.rkt
@@ -14,19 +14,20 @@
                                   y)) null sequence))
 
 ;append
+(define  (append1 lst1 lst2)
+  (accumulate (lambda (x y) (if (null? x) y (cons x y)))
+              lst2 lst1))
+
 (define  (append lst1 lst2)
-  (accumulate (lambda (x y) 
-                (if (null? x)
-                    y
-                    (cons x y))) lst2 lst1))
+  (accumulate cons lst2 lst1))
 
 ;length
 (define (length lst)
-  (accumulate (lambda (x y) (+ (if (null? x) 0 1) y)) 0 lst))
+  (accumulate (lambda (x y) (+ 1 y)) 0 lst))
 
 ;test
-(map (lambda (x) (* x x)) '(1 2 3)) 
+;(map (lambda (x) (* x x)) '(1 2 3)) 
 
-(append '(1 2) '(3 4))
+;(append '(1 2) '(3 4))
 
-(length '(1 2 3))
+;(length '(1 2 3))
