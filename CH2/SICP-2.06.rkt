92be72154cd762575e3bd31a857d998ce3254544
GTxx <xiongxiong1986@gmail.com>
2013-08-27 11:13:50 +0800

    church mul

--- CH2/SICP-2.06.rkt
+++ CH2/SICP-2.06.rkt
@@ -9,9 +9,15 @@
 (define (add n1 n2)
   (lambda (f) (lambda (x) ((n1 f) ((n2 f) x)))))
 
+(define (mul n1 n2)
+  (lambda (f) (n1 (n2 f))))
+
 (define one (add1 zero))
 
 (define two (add one one))
 
+(define four (mul two two))
 ;;test 
 (define test ((two (lambda (x) (* x x))) 3))
+
+(define test1 ((four (lambda (x) (+ 1 x))) 0))
